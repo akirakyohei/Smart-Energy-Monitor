6bb8f68243078fde64ffbd184c516cfb838be0cb
Huy Dong <huydong.hoanam@gmail.com>
2021-12-19 14:37:26 +0700

    refacter code python

---
+++ sem-ml-app/python-code/predict/metter_day_of_month.py
@@ -0,0 +1,5 @@
+# trong file nay chi co hamf predict duy nhat 
+def predict(input):
+# ..... viet code du doan vao day
+#xin vi du ve dau vao va dau ra ntn 
+    return 'result'
---
+++ sem-ml-app/python-code/predict/metter_day_of_week.py
@@ -0,0 +1,6 @@
+
+# trong file nay chi co hamf predict duy nhat 
+def predict(input):
+# ..... viet code du doan vao day
+#xin vi du ve dau vao va dau ra ntn 
+    return 'result'
